336fc98f0e4997d84dc87e193f0935f61874b90b
Theizerg <theizerg@gmail.com>
2021-12-08 13:59:53 -0400

    procfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,29 +1,39 @@
 <?php
 
-namespace App\Providers;
+namespace App\Providers; 
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
+   /**
      * Register any application services.
      *
      * @return void
      */
     public function register()
     {
-         \Schema::defaultStringLength(500);
-       \Carbon\Carbon::setToStringFormat('d-m-Y h:i A');
+        \Schema::defaultStringLength(350);
+        \Carbon\Carbon::setToStringFormat('d-m-Y h:i A');
+
+        if (env('REDIRECT_HTTPS')) {
+             $this->app['request']->server->set('HTTPS', true);
+         }
     }
+
+   
 
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (env('REDIRECT_HTTPS')) {
+           $url->formatScheme('https://');
+       }
+    }
     }
 }
